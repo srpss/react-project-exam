2519d7f9b371ac8a05a2e4780320ae726a416f37
srpss <martinslavovforfut1@gmail.com>
2022-08-05 18:22:50 +0300

    client- create cleanup

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -4,62 +4,35 @@ export default function Create() {
   const onSubmit = (e) => {
     e.preventDefault();
 
-    const gameData = Object.fromEntries(new FormData(e.target));
+    const boardData = Object.fromEntries(new FormData(e.target));
 
-    console.log(gameData);
+    console.log(boardData);
 
-    addGameHandler(gameData)
-};
+
+  };
 
   return (
-      <section id="create-page" className="auth">
-          <form id="create" onSubmit={onSubmit}>
-              <div className="container">
-                  <h1>Create Game</h1>
-                  <label htmlFor="leg-title">Legendary title:</label>
-                  <input
-                      type="text"
-                      id="title"
-                      name="title"
-                      placeholder="Enter game title..."
-                  />
-                  <label htmlFor="category">Category:</label>
-                  <input
-                      type="text"
-                      id="category"
-                      name="category"
-                      placeholder="Enter game category..."
-                  />
-                  <label htmlFor="levels">MaxLevel:</label>
-                  <input
-                      type="number"
-                      id="maxLevel"
-                      name="maxLevel"
-                      min={1}
-                      placeholder={1}
-                  />
-
-                  <label htmlFor="game-img">Image:</label>
-
-                  <input
-                      type="text"
-                      id="imageUrl"
-                      name="imageUrl"
-                      placeholder="Upload a photo..."
-                  />
-
-                  <label htmlFor="summary">Summary:</label>
-
-                  <textarea name="summary" id="summary" defaultValue={""} />
-
-                  <input
-                      className="btn submit"
-                      type="submit"
-                      value="Create Game"
-                  />
-              </div>
-          </form>
-      </section>
+    <section id="create-page" className="auth">
+      <form id="create" onSubmit={onSubmit}>
+        <div className="container">
+          <h1>Create board thread</h1>
+          <label htmlFor="leg-title">Board original post:</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            placeholder="How it will start"
+          ></input>
+          <label htmlFor="single-img">Image:</label>
+          <input
+            type="text"
+            id="imageUrl"
+            name="imageUrl"
+            placeholder="Image URL https:/...png"
+          />
+        </div>
+      </form>
+    </section>
   );
-  
+
 }
