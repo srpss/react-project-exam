6a9aef3fcc747360c5c0ac23bca7c2b2a1474b49
srpss <martinslavovforfut1@gmail.com>
2022-08-06 19:22:28 +0300

    server side Register bug fixes

--- server/app/middlewares/verifySignUp.js
+++ server/app/middlewares/verifySignUp.js
@@ -17,22 +17,7 @@ checkDuplicateUsernameOrEmail = (req, res, next) => {
       return;
     }
 
-    // Email
-    User.findOne({
-      email: req.body.email
-    }).exec((err, user) => {
-      if (err) {
-        res.status(500).send({ message: err });
-        return;
-      }
-
-      if (user) {
-        res.status(400).send({ message: "Failed! Email is already in use!" });
-        return;
-      }
-
-      next();
-    });
+   next();
   });
 };
 
