acb13a03d60f6059d1d08a18067c2f22908eb3ed
srpss <martinslavovforfut1@gmail.com>
2022-08-07 16:39:59 +0300

    Logout functionality complete

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,22 +1,19 @@
-import React from 'react'
+
+import { useEffect } from 'react';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Context } from './context/Context';
 
 export default function Logout() {
 
-  const navigate = useNavigate();
-    const { user, userLogout } = useContext(AuthContext);
-
+    const navigate = useNavigate();
+    const { user, userLogout } = useContext(Context);
+    console.log(user)
     useEffect(() => {
-        authService.logout(user.accessToken)
-            .then(() => {
-                userLogout();
-                navigate('/');
-            })
-            .catch(() => {
-                navigate('/');
-            });
-    });
+        userLogout();
+        navigate('/');
+    },[])
 
     return null;
 }
+
